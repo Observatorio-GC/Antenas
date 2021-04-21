c90837f9e4553d379cae9d369149799473de71df
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2021-04-21 12:35:28 -0300

    Delete Cajeros.js

--- scripts/Cajeros.js
+++
@@ -1,8 +0,0 @@
-var Cajeros = {
-"type": "FeatureCollection",
-"name": "Cajeros",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": null, "Nombre_1": "Banco Supervielle", "Descrip": "Montes de Oca 76", "id_2": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.864709684476864, -32.922877398268376 ] ] } }
-]
-}
