c7672af16bb3399c78bc74e3203b4af64ba96ed8
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2021-04-21 13:15:50 -0300

    Add files via upload

---
+++ scripts/Hospital.js
@@ -0,0 +1,8 @@
+var Hospital = {
+"type": "FeatureCollection",
+"name": "Hospital",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "qc_id": 1, "dpto": "Godoy Cruz", "distrito": "Ciudad", "direccion": "Talcahuano S\/N", "telefono": "4272600 \/ 4272773", "complejida": "II MONOVALENTE", "n_camas": "132", "especialid": "ENF.END\/EP", "servicio": "INTER. Y CONS. EX", "responsabl": "Lic. Luis Ten Huver", "Nombre_1": "Hospital Publico Lencinas", "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.870817296300288, -32.903574896312243 ] ] } }
+]
+}
