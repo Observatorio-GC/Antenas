903d94f530a68ae1f79cbc62d1c101891c2cc70f
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2021-04-21 12:57:47 -0300

    Add files via upload

---
+++ scripts/CicloviasExistentes.js
@@ -0,0 +1,9 @@
+var CicloviasExistentes = {
+"type": "FeatureCollection",
+"name": "CicloviasExistentes",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Distancia": "ERNESTO CONTRERAS (2,4 Km)", "Id": 3, "TIPO_2": "CICLOVIA", "fer": 2401, "id_2": 1 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -68.86482548098428, -32.924252678674485 ], [ -68.863764333959324, -32.92372833255066 ] ] ] } },
+{ "type": "Feature", "properties": { "Distancia": "BULEVAR LAGO HERMOSO (0,82 Km)", "Id": 9, "TIPO_2": "CICLOVIA", "fer": 826, "id_2": 1 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -68.8666702, -32.9459950991385 ], [ -68.869921, -32.945472899138402 ], [ -68.8720561, -32.945094699138401 ], [ -68.8753391, -32.944563499138503 ] ] ] } }
+]
+}
