19fb28156afa09a958d9918d6f98273af1119568
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2021-04-21 11:39:07 -0300

    Add files via upload

---
+++ scripts/Polideportivos.js
@@ -0,0 +1,17 @@
+ var Polideportivos = {
+"type": "FeatureCollection",
+"name": "Polideportivos",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nombre_1": "POLIDEPORTIVO N° 2  (C.E.C.)", "UBICACION": "Juncal y Rio Gallegos", "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.872394064614952, -32.909438536592411, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "POLIDEPORTIVO Nº 5 (LA ESTANZUELA)", "UBICACION": "Lorenzo Soler y Arturo Illia BÂº La Estanzuela", "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.868617151542423, -32.941158005719188, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "POLIDEPORTIVO N° 8 (CECOM)", "UBICACION": null, "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.867243677115397, -32.905935235535701, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "POLIDEPORTIVO N° 9 (BÂ° EL CARDENA)", "UBICACION": "(Las Tunas y Africa, BÂº El Cardenal)\n", "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.865239640692167, -32.936148465516652, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "POLIDEPORTIVO N°10 (AZUL Y GRANA)", "UBICACION": "(Salvador Civit y Los Aguaribay, BÂº RuiseÃ±or)\n", "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.864314694328144, -32.932602766543141, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "POLIDEPORTIVO N° 13 (LOS PARAISOS)", "UBICACION": "( Los Aguaribay y Rivadavia)\n", "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.864090472566389, -32.925617037775673, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "POLIDEPORTIVO N° 17 (PREDIO AFAG)", "UBICACION": "(Lago Hermoso y Olmo Zarate)\n", "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.869648913980086, -32.946313776442366, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "POLIDEPORTIVO N° 20 (CHUQUISACA)", "UBICACION": "(Chuquisaca y Pedro Zanni)\n", "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.872985092069825, -32.919828664621988, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "MICRO ESTADIO DE HOCKEY N° 21 (LIGA 8)", "UBICACION": "(Talcahuano y Las Tipas)\n", "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.867992098082027, -32.903690198535131, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "POLIDEPORTIVO N° 14 (SANTIAGO F. LLAVER)", "UBICACION": "(Pte Arturo Illia y Cipolletti, BÂ° Eva PerÃ³n)\n", "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.869187833153674, -32.934504082288768, 0.0 ] ] } }
+]
+}
---
+++ scripts/PuntosVerdes.js
@@ -0,0 +1,24 @@
+var PuntosVerdes = {
+"type": "FeatureCollection",
+"name": "PuntosVerdes",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre_1": "Boulevard San Vicente esquina Talcahuano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.876921971439714, -32.901832777259983 ] ] } },
+{ "type": "Feature", "properties": { "id": 4, "Nombre_1": "Colegio Victoria Ocampo" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.880136322307521, -32.913292861681377 ] ] } },
+{ "type": "Feature", "properties": { "id": 5, "Nombre_1": "Colegio Cordillera de los Andes" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.875741242974144, -32.921824312752726 ] ] } },
+{ "type": "Feature", "properties": { "id": 28, "Nombre_1": "Polideportivo Barrio Supe" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.889630891420111, -32.900273737947686 ] ] } },
+{ "type": "Feature", "properties": { "id": 36, "Nombre_1": "Polideportivo N 8 (CECOM)" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.866945994541027, -32.905856809888 ] ] } },
+{ "type": "Feature", "properties": { "id": 44, "Nombre_1": "Estadio de Hockey" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.867569757708296, -32.903114792664972 ] ] } },
+{ "type": "Feature", "properties": { "id": 51, "Nombre_1": "Plaza Fray Mamerto Esquiu" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.879217907989684, -32.913587970041306 ] ] } },
+{ "type": "Feature", "properties": { "id": 52, "Nombre_1": "Polideportivo Chuquisaca" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.872968243062758, -32.919614989107195 ] ] } },
+{ "type": "Feature", "properties": { "id": 67, "Nombre_1": "Cerede 2" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.872614716360644, -32.909862383338229 ] ] } },
+{ "type": "Feature", "properties": { "id": 82, "Nombre_1": "Pres. Illia y Dique Llos Nihuiles" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.870315190381689, -32.937063481412487 ] ] } },
+{ "type": "Feature", "properties": { "id": 83, "Nombre_1": "Playón Deportivo Ruiseñor" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.864260533002707, -32.932322308439467 ] ] } },
+{ "type": "Feature", "properties": { "id": 101, "Nombre_1": "Polideportivo N 5" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.872320502764012, -32.942540403341681 ] ] } },
+{ "type": "Feature", "properties": { "id": 106, "Nombre_1": "Jockey Club" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.868882936797377, -32.914992875105554 ] ] } },
+{ "type": "Feature", "properties": { "id": 110, "Nombre_1": "Montes de Oca y Blanco Encalada" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.871049476351672, -32.911113580580107 ] ] } },
+{ "type": "Feature", "properties": { "id": 111, "Nombre_1": "Super Vea Chuquisaca y América" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.876423972446659, -32.912451602734507 ] ] } },
+{ "type": "Feature", "properties": { "id": 112, "Nombre_1": "Union Vecinal La Estanzuela" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.869000772288032, -32.945730526604336 ] ] } },
+{ "type": "Feature", "properties": { "id": 114, "Nombre_1": "Plaza de los Niños" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.870789785935401, -32.937586076292995 ] ] } }
+]
+}
